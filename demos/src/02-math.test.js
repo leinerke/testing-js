9bb1724f26a4204c6cc1ecfe3254e03aee59b19a
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-20 17:48:02 -0500

    Implementado Setup.

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,18 +1,26 @@
 const { divide, multiply, sum } = require('./02-math');
 
-test('add 1 + 3 should be 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe('Test for Math', () => {
+  describe('Test for sum', () => {
+    test('add 1 + 3 should be 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('add 2 * 3 should be 6', () => {
-  const rta = multiply(1, 3);
-  expect(rta).toBe(4);
-});
+  describe('Test for multiply', () => {
+    test('add 2 * 3 should be 6', () => {
+      const rta = multiply(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test('add 2 / 2 should be 1', () => {
-  const rta = divide(2, 2);
-  expect(rta).toBe(4);
-  const rta2 = divide(6, 0);
-  expect(rta2).toBeNull();
+  describe('Test for divide', () => {
+    test('add 2 / 2 should be 1', () => {
+      const rta = divide(2, 2);
+      expect(rta).toBe(4);
+      const rta2 = divide(6, 0);
+      expect(rta2).toBeNull();
+    });
+  });
 });
