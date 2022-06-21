7a3f72c94a96356150ce36b0574a651f3139c3d6
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-20 21:00:45 -0500

    Spies

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -11,14 +11,21 @@ const fakeBooks = [
   },
 ];
 
+const mockGetAll = jest.fn();
+
+/*
 const MongoLibStub = {
-  getAll: () => fakeBooks,
+  getAll: spyGetAll,
   create: () => {},
 };
+ */
 
 jest.mock(
   '../lib/mongo.lib',
-  () => jest.fn().mockImplementation(() => MongoLibStub),
+  () => jest.fn().mockImplementation(() => ({
+    getAll: mockGetAll,
+    create: () => {},
+  })),
 );
 
 describe('Test for BooksService', () => {
@@ -31,20 +38,30 @@ describe('Test for BooksService', () => {
   describe('Test for getBooks', () => {
     test('Should return a list books', async () => {
       // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
-      const books = await service.getBooks();
+      const books = await service.getBooks({});
       console.log(books);
       // Assert
       expect(books.length).toEqual(2);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
 
     test('Should return a list books', async () => {
       // Arrange
+      mockGetAll.mockResolvedValue([
+        {
+          _id: '62b11a8a1946f8e5c2e20698',
+          name: 'harry popoter y la piedra filosofal',
+        },
+      ]);
       // Act
       const books = await service.getBooks();
       console.log(books);
       // Assert
-      expect(books[0].name).toEqual('harry popoter');
+      expect(books.length).toEqual(1);
     });
   });
 });
